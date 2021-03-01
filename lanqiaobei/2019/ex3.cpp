2c78d89660a99a4ed2ba87e57dd65fd4709779d1
waitthenexttime <1198664769@qq.com>
2021-03-01 11:56:36 +0800

    1

---
+++ lanqiaobei/2019/ex3.cpp
@@ -0,0 +1,25 @@
+/* 
+
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int a = 1, b = 1, c = 1, res;
+    for (int i = 4; i <= 20190324; i++)
+    {
+        res = (a+b+c)%10000;
+        a=b;
+        b=c;
+        c=res;
+    }
+    cout<<res;
+    cout << endl;
+    system("pause");
+    return 0;
+}
