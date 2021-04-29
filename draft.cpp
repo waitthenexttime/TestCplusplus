f815e11ed0f47996fbc9107b547c26335cc48950
waitthenexttime <1198664769@qq.com>
2021-04-29 22:09:35 +0800

    1

--- draft.cpp
+++ draft.cpp
@@ -6,13 +6,18 @@
 #include <algorithm>
 #include <vector>
 #include <cmath>
+#include <numeric>
 #include <cstring>
 #include <unordered_map>
 using namespace std;
 
 int main()
 {
- 
+    vector<int> a = {1, 2, 3};
+    vector<int> b = {4, 5, 6};
+    auto p1 = a.end();
+    cout<<*p1;
+    stoi()
     cout << endl;
     system("pause");
     return 0;
